ac55e1497931b4c898ef19f373602ec69a55139c
NB <47492535+altmshfkgudtjr@users.noreply.github.com>
2020-11-08 17:45:44 +0900

    모바일 포스트 가독성 개선

--- src/components/post/PostLinkWrapper.tsx
+++ src/components/post/PostLinkWrapper.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+// lib
+import media from 'lib/styles/media'
 
 interface PostLinkWrapperProps {
 	postId: string;
@@ -24,9 +26,16 @@ const Container = styled.a<ContainerStyled>`
 	display: flex;
 	flex-direction: column;
 	padding: ${props => props.isList
-		? '0'
+		? '.5rem'
 		: '1rem 1rem .5rem 1rem'
 	};
+
+	${media.small} {
+		padding: ${props => props.isList
+			? '0'
+			: '.5rem 0'
+		};
+	}
 `;
 
 export default PostLinkWrapper
